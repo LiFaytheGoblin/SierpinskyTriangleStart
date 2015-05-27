fb040650e49e862e416915d94d3133ac54e7a0b8
B Kleinen <kleinen@infrastructure.de>
2015-05-27 09:08:31 +0200

    updated

--- src/triangle/SierpinskiTriangle.java
+++ src/triangle/SierpinskiTriangle.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Polygon;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
